0a86934e52702d5cfe17d322299baa06420a246c
Git Server Admin <admin@git-server>
2021-02-27 19:23:51 +0000

    Add initial repository

---
+++ CompanyService.WebAPI/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CompanyService.WebAPI.Models
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+
+        public int NumberOfEmployees { get; set; }
+
+        public int AverageSalary { get; set; }
+    }
+}
